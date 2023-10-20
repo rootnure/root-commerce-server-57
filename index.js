ae564ff1dc7b07f7be2ba22fc2178c2d8b7f8c35
rootnure <nur.diu.2791@gmail.com>
2023-10-20 09:31:18 +0600

    GET for a single product added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,7 +41,15 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
             // res.send(cursor);
-        })
+        });
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const cursor = productsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
 
 
